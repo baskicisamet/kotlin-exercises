be707e45d8fee3797157568310dc00234611a3a5
samet <samet@bumin.io>
2018-10-25 11:04:39 +0300

    init

---
+++ src/main/kotlin/com/sam/example/kotlinexercises/KotlinExercisesApplication.kt
@@ -0,0 +1,11 @@
+package com.sam.example.kotlinexercises
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinExercisesApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinExercisesApplication>(*args)
+}
